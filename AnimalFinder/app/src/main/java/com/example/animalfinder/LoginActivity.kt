966e2b35a691d03d8f74d3325236b9223557afd8
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-04-14 23:29:34 -0300

    connecting app with firebase and finishing authentication

--- AnimalFinder/app/src/main/java/com/example/animalfinder/LoginActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/LoginActivity.kt
@@ -2,10 +2,36 @@ package com.example.animalfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
+private const val TAG = "LoginActivity"
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this,"Email/Password cant be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    goHomeActivity()
+                } else {
+                    Log.i(TAG, "signInWithEmail failed", task.exception)
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun goHomeActivity() {
+        Log.i(TAG, "goHomeActivity")
     }
 }
