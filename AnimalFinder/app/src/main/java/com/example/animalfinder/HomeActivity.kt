7f61e557fb58ed5f862e980c91b0a4e1b7f9ff08
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-04-17 22:37:08 -0300

    creating Homeactivity and profile activity

---
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.animalfinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.navigation_profile) {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- AnimalFinder/app/src/main/java/com/example/animalfinder/LoginActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.example.animalfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.animalfinder.ui.home.HomeFragment
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_dashboard.*
@@ -13,19 +15,24 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            goHomeActivity()
+        }
         btnLogin.setOnClickListener {
+            btnLogin.isEnabled = false
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
             if (email.isBlank() || password.isBlank()) {
                 Toast.makeText(this,"Email/Password cant be empty", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            val auth = FirebaseAuth.getInstance()
             auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                btnLogin.isEnabled = false
                 if (task.isSuccessful) {
                     goHomeActivity()
                 } else {
-                    Log.i(TAG, "signInWithEmail failed", task.exception)
+                    Log.e(TAG, "signInWithEmail failed", task.exception)
                     Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -33,5 +40,8 @@ class LoginActivity : AppCompatActivity() {
     }
     private fun goHomeActivity() {
         Log.i(TAG, "goHomeActivity")
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
