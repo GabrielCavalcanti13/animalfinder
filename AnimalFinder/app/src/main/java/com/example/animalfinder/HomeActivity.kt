ea2fa5b26c68aa81417141f53487272187f8cb4d
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-04-20 00:52:56 -0300

    connecting firestore querying and retrieving data

--- AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
@@ -3,13 +3,31 @@ package com.example.animalfinder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
 
+private const val TAG = "HomeActivity"
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var firestoredb: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        firestoredb = FirebaseFirestore.getInstance()
+        val postsReference = firestoredb.collection("posts")
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.e(TAG, "Exception when querying photos", exception)
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents) {
+                Log.i(TAG, "Document ${document.id}: ${document.data}")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
