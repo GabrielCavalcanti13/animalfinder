a1d28bfa4e12ebc4e33106f460987e8dd4a6e2dd
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-04-21 12:30:28 -0300

    displaying list of photos from all user on homeactivity

--- AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
@@ -6,18 +6,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.animalfinder.models.Post
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_home.*
 
 private const val TAG = "HomeActivity"
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var firestoredb: FirebaseFirestore
+    private lateinit var posts: MutableList<Post>
+    private lateinit var adapter: PostsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        posts = mutableListOf()
+        adapter = PostsAdapter(this, posts)
+        rvPosts.adapter = adapter
+        rvPosts.layoutManager  = LinearLayoutManager(this)
 
         firestoredb = FirebaseFirestore.getInstance()
         val postsReference = firestoredb
@@ -30,6 +39,9 @@ class HomeActivity : AppCompatActivity() {
                 return@addSnapshotListener
             }
             val postList = snapshot.toObjects(Post::class.java)
+            posts.clear()
+            posts.addAll(postList)
+            adapter.notifyDataSetChanged()
             for (post in postList) {
                 Log.i(TAG, "Post ${post}")
             }
