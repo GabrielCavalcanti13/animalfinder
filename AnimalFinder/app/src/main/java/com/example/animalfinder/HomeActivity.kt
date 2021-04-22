6952a9d12a514b970fcf4ee491f0db07105f0a2c
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-04-21 21:56:41 -0300

    showing only photos from signed user in profile page

--- AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/HomeActivity.kt
@@ -8,13 +8,17 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.animalfinder.models.Post
+import com.example.animalfinder.models.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_home.*
 
 private const val TAG = "HomeActivity"
-class HomeActivity : AppCompatActivity() {
+private const val EXTRA_USERNAME = "EXTRA_USERNAME"
+open class HomeActivity : AppCompatActivity() {
 
+    private var signedUser: User? = null
     private lateinit var firestoredb: FirebaseFirestore
     private lateinit var posts: MutableList<Post>
     private lateinit var adapter: PostsAdapter
@@ -29,10 +33,22 @@ class HomeActivity : AppCompatActivity() {
         rvPosts.layoutManager  = LinearLayoutManager(this)
 
         firestoredb = FirebaseFirestore.getInstance()
-        val postsReference = firestoredb
+        firestoredb.collection("users").document(FirebaseAuth.getInstance().currentUser?.uid as String)
+                .get().addOnSuccessListener { userSnapshot ->
+                    signedUser = userSnapshot.toObject(User::class.java)
+                }
+
+        var postsReference = firestoredb
                 .collection("posts")
                 .limit(15)
                 .orderBy("post_time_ms", Query.Direction.DESCENDING)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        if (username != null) {
+            supportActionBar?.title = username
+            postsReference = postsReference.whereEqualTo("user.username", username)
+        }
+
         postsReference.addSnapshotListener { snapshot, exception ->
             if (exception != null || snapshot == null) {
                 Log.e(TAG, "Exception when querying photos", exception)
@@ -56,6 +72,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.navigation_profile) {
             val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra(EXTRA_USERNAME, signedUser?.username)
             startActivity(intent)
         }
         return super.onOptionsItemSelected(item)
--- AnimalFinder/app/src/main/java/com/example/animalfinder/ProfileActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/ProfileActivity.kt
@@ -10,26 +10,22 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
 private const val TAG = "ProfileActivity"
-class ProfileActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-        btn_logout.setOnClickListener {
-            Log.i(TAG, "User is logging out")
-            FirebaseAuth.getInstance().signOut()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-    }
+class ProfileActivity : HomeActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.home_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.navigation_home) {
             val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        if (item.itemId == R.id.logout) {
+            Log.i(TAG, "User is logging out")
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
         return super.onOptionsItemSelected(item)
