c519f74063b1f572a2b25dfbba57b68f8a1041d0
GabrielCavalcanti13 <gcm2@cin.ufpe.br>
2021-05-01 14:09:48 -0300

    Implemented feature to get current location

--- AnimalFinder/app/src/main/java/com/example/animalfinder/CreateActivity.kt
+++ AnimalFinder/app/src/main/java/com/example/animalfinder/CreateActivity.kt
@@ -1,19 +1,25 @@
 package com.example.animalfinder
 
 import android.app.Activity
-import android.app.Instrumentation
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.animalfinder.models.Post
 import com.example.animalfinder.models.User
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_create.*
+import java.util.*
 
 private const val PICK_PHOTO_CODE = 123
 class CreateActivity : AppCompatActivity() {
@@ -21,11 +27,13 @@ class CreateActivity : AppCompatActivity() {
     private var signedUser: User? = null
     private lateinit var firestoredb: FirebaseFirestore
     private lateinit var storageReference: StorageReference
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         storageReference = FirebaseStorage.getInstance().reference
         firestoredb = FirebaseFirestore.getInstance()
         firestoredb.collection("users").document(FirebaseAuth.getInstance().currentUser?.uid as String)
@@ -33,6 +41,7 @@ class CreateActivity : AppCompatActivity() {
                     signedUser = userSnapshot.toObject(User::class.java)
                 }
 
+        getLocation()
         btnSelectImage.setOnClickListener {
             val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
             imagePickerIntent.type = "image/*"
@@ -64,7 +73,7 @@ class CreateActivity : AppCompatActivity() {
             val post = Post(
                 etDescription.text.toString(),
                 etSpecie.text.toString(),
-                "Recife - Brazil",
+                etLocation.text.toString(),
                 downloadUrlTask.result.toString(),
                 System.currentTimeMillis(),
                 signedUser)
@@ -92,6 +101,45 @@ class CreateActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 photo = data?.data
                 imageView.setImageURI(photo)
+            }
+        }
+    }
+
+    private fun getLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+        var geocoder : Geocoder
+        geocoder = Geocoder(this, Locale.getDefault())
+
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                        .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+                val addressList = geocoder.getFromLocation(it.latitude, it.longitude, 10) as ArrayList<Address>
+                val addressName = addressList[0].getAddressLine(0)
+                println(addressName)
+                val country = addressList[0].countryName
+                var city = addressList[0].locality
+                if (city == null && addressList.size > 0) {
+                    for (adr in addressList) {
+                        if (adr.locality != null && adr.locality.isNotEmpty()) {
+                            city = adr.locality
+                        }
+                    }
+                }
+                var photoLocation = ""
+                if (city == null) {
+                    photoLocation = country
+                } else {
+                    photoLocation = "$city - $country"
+                }
+                etLocation.setText(photoLocation)
             }
         }
     }
